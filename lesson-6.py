6edfa26e1c8b09b893ccaf586b048c1159b4c477
Anton <anton.antonov1579@gmail.com>
2020-02-11 20:06:57 +0300

    lesson 6.3

--- lesson-6.py
+++ lesson-6.py
@@ -55,6 +55,52 @@ class Road:
 road = Road(20, 5000)
 road.mass()
 
+# 3. Реализовать базовый класс Worker (работник), в котором определить атрибуты:
+# name, surname, position (должность), income (доход). Последний атрибут должен
+# быть защищенным и ссылаться на словарь, содержащий элементы: оклад и премия, например,
+# {"wage": wage, "bonus": bonus}. Создать класс Position (должность) на базе класса Worker.
+# В классе Position реализовать методы получения полного имени сотрудника (get_full_name)
+# и дохода с учетом премии (get_total_income). Проверить работу примера на реальных данных
+# (создать экземпляры класса Position, передать данные, проверить значения атрибутов,
+# вызвать методы экземпляров).
+
+class Worker:
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {'wage': wage, 'bonus': bonus}
+
+class Possition(Worker):
+
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return self.name + '-' + self.surname
+
+    def get_total_income(self):
+        return self._income.get('wage') + self._income.get('bonus')
+
+
+user = Possition('Marat', 'Popov', 'Designer', 50000, 8000)
+print(f' Name: {user.get_full_name()} \n Position: {user.position} \n Salary: {user.get_total_income()}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
